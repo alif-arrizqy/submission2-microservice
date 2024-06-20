2ce0900f2e7f883872cc442e622fa1fe63d58080
alif-arrizqy <alif.arrizqy20@gmail.com>
2024-06-20 10:01:12 +0700

    build packages using ghcr

--- karsajobs-ui/build_push_image_karsajobs_ui.sh
+++ karsajobs-ui/build_push_image_karsajobs_ui.sh
@@ -8,10 +8,14 @@ docker build -t karsajobs-ui:latest .
 docker images
 
 # change image name
-docker tag karsajobs-ui:latest arrizqydev/karsajobs-ui:latest
+# docker tag karsajobs-ui:latest arrizqydev/karsajobs-ui:latest
+docker tag karsajobs-ui:latest ghcr.io/alif-arrizqy/karsajobs-ui:latest
 
 # login to docker hub
-echo $PASSWORD_DOCKER_HUB | docker login -u arrizqydev --password-stdin
+# echo $PASSWORD_DOCKER_HUB | docker login -u arrizqydev --password-stdin
+echo $DOCKER_GHCR_PASSWORD | docker login ghcr.io -u alif-arrizqy --password-stdin
+
 
 # push image to docker hub
-docker push arrizqydev/karsajobs-ui:latest
+# docker push arrizqydev/karsajobs-ui:latest
+docker push ghcr.io/alif-arrizqy/karsajobs-ui:latest
--- karsajobs/build_push_image_karsajobs.sh
+++ karsajobs/build_push_image_karsajobs.sh
@@ -8,10 +8,14 @@ docker build -t karsajobs:latest .
 docker images
 
 # change image name
-docker tag karsajobs:latest arrizqydev/karsajobs:latest
+# docker tag karsajobs:latest arrizqydev/karsajobs:latest
+docker tag karsajobs:latest ghcr.io/alif-arrizqy/karsajobs:latest
 
 # login to docker hub
-echo $PASSWORD_DOCKER_HUB | docker login -u arrizqydev --password-stdin
+# echo $PASSWORD_DOCKER_HUB | docker login -u arrizqydev --password-stdin
+echo $DOCKER_GHCR_PASSWORD | docker login ghcr.io -u alif-arrizqy --password-stdin
+
 
 # push image to docker hub
-docker push arrizqydev/karsajobs:latest
+# docker push arrizqydev/karsajobs:latest
+docker push ghcr.io/alif-arrizqy/karsajobs:latest
